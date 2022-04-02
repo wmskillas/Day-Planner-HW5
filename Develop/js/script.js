9012486e488a4da1eeec8aacd48a3a5112433124
William Skillas <wmskillas@gmail.com>
2022-04-02 00:36:47 -0400

    added javascript functionality and some css stylizing

--- Develop/js/script.js
+++ Develop/js/script.js
@@ -1,8 +1,68 @@
 var today = moment();
 $("#currentDay").text(today.format("dddd, MMMM Do, YYYY"));
 
+$(document).ready(function() {
+$(".saveBtn").on("click", function () {
 
-var nine = moment('9:00am', 'h:mm a').format('hh:mm a');
+    var written = $(this).siblings(".description").val();
+    var currentTime = $(this).parent().attr("id");
+    console.log(written);
+    console.log(currentTime);
+
+    localStorage.setItem(currentTime, written);
+})
+//I was trying to see if i could have multiple ways to save the written text. But having 2 of the functions seemed to mess with some other stuff and not actually save
+/*$(document).ready(function() {
+    $(".saveBtn").on("submit", function () {
+    
+        var written = $(this).siblings(".description").val();
+        var currentTime = $(this).parent().attr("id");
+        console.log(written);
+        console.log(currentTime);
+    
+        localStorage.setItem(currentTime, written);
+    })*/
+
+function timeKeeper(){
+    var currentTime = moment().hour();
+    console.log(currentTime);
+
+    $('.time-block').each(function (){
+        var timeBlock = parseInt($(this).attr('id').split('hour')[1]);
+        console.log(timeBlock);
+
+        if(timeBlock === currentTime){
+            $(this).removeClass('past');
+            $(this).removeClass('future')
+            $(this).addClass('present')
+        }
+        else if (timeBlock < currentTime) {
+            $(this).removeClass('present');
+            $(this).removeClass('future')
+            $(this).addClass('past')
+        }
+        else {
+            $(this).removeClass('past');
+            $(this).removeClass('present')
+            $(this).addClass('future')
+        }
+    })
+}
+
+$('#nine .description').val(localStorage.getItem('nine'));
+$('#ten .description').val(localStorage.getItem('ten'));
+$('#eleven .description').val(localStorage.getItem('eleven'));
+$('#twelve .description').val(localStorage.getItem('twelve'));
+$('#one .description').val(localStorage.getItem('one'));
+$('#two .description').val(localStorage.getItem('two'));
+$('#three .description').val(localStorage.getItem('three'));
+$('#four .description').val(localStorage.getItem('four'));
+$('#five .description').val(localStorage.getItem('five'));
+
+timeKeeper();
+})
+//I was trying to use moment.js to show the times but i couldn't figure out how to put these in the functions
+/*var nine = moment('9:00am', 'h:mm a').format('hh:mm a');
 $('#nine').text(nine);
 
 var ten = moment('10:00am', 'hh:mm a').format('hh:mm a');
@@ -27,10 +87,4 @@ var four = moment('4:00pm', 'h:mm a').format('hh:mm a');
 $('#four').text(four);
 
 var five = moment('5:00pm', 'h:mm a').format('hh:mm a');
-$('#five').text(five);
-
-
-var currentTime = moment().format('LT');
-console.log(currentTime);
-
-//for (var i = 0, i <  )
+$('#five').text(five); */
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Day-Planner-HW5
+-The Day-Planner shows a normal workday of hours and allows you to type and save it locally. This allows you to close the page and reopen and it show the text that you had previously written in the same time slot.
+## API's
+-This homework uses moment.js, to get time information, and jquery,for simpler coding and statements, for javascript
+-And uses bootstrap, google fonts, and font awesome for css stylizing
+## Problems
+I couldn't figure out how to get rid of the color bleed into the time and save button from the text area.
+
+## Site and Gif
